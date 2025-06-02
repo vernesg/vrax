4134584a4f54b89bc6703729dcaaaf04d3913b2c
vernesg <veaxdev36@gmail.com>
2025-06-02 23:43:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Vrax is running!');
+});
+
 const port = process.env.PORT || 10000;
 
 app.listen(port, () => {
