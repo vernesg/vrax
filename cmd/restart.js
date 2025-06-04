704aefcbd227c0d422e9c7f0cd0b10fad920860b
vernesg <veaxdev36@gmail.com>
2025-06-04 12:15:54 +0800

    Delete cmd directory

--- cmd/restart.js
+++
@@ -1,9 +0,0 @@
-module.exports.restart = {
-    name: 'restart',
-    description: 'Reboot the bot',
-    execute(api, event, args) {
-        api.sendMessage('Rebooting Please wait...', event.threadID, () => {
-            process.exit(0);
-        });
-    }
-};
